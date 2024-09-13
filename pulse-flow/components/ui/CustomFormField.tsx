29fec1091261a97286a70b77bdeab2b5bb20bc64
messat <148489126+messat@users.noreply.github.com>
2024-09-13 22:19:21 +0100

    Working on customFieldForm to be dynamic form

---
+++ pulse-flow/components/ui/CustomFormField.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+  } from "@/components/ui/form"
+  
+import { Input } from "@/components/ui/input"
+import { Control } from 'react-hook-form'
+import { FormFieldType } from '../forms/PatientForms'
+
+interface CustomProps {
+    control: Control<any>,
+    fieldType: FormFieldType,
+    name: string,
+    label?: string,
+    placeholder?:string,
+    iconSrc?: string,
+    iconAlt?: string,
+    disabled?: boolean,
+    dateFormat?: string,
+    showTimeSelect?: boolean,
+    children?: React.ReactNode,
+    renderSkeleton?: (field: any)=> React.ReactNode
+}
+
+
+
+
+const CustomFormField = ({control, fieldType, name, label, }: CustomProps) => {
+  return (
+    <FormField
+        control={control}
+        name="username"
+        render={({ field }) => (
+            <FormItem className='flex-1'>
+            {fieldType !== FormFieldType.CHECKBOX && label && (
+                <FormLabel>{label}</FormLabel>
+            )}
+
+            </FormItem>
+        )}
+      />
+  )
+}
+
+export default CustomFormField
